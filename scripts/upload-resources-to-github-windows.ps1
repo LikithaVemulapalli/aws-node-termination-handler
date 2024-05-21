8a7ad70979ae9b730679660066020d55e82ca164
ec2-bot 🤖 <ec2-bot@users.noreply.github.com>
2024-05-20 19:38:46 -0700

    Used Write-Output

--- scripts/upload-resources-to-github-windows.ps1
+++ scripts/upload-resources-to-github-windows.ps1
@@ -5,25 +5,25 @@ param(
     [switch]$K8sAssetsOnly
 )
 
-$ErrorActionPreference = "Stop"
+#$ErrorActionPreference = "Stop"
 
-# Function to handle errors and cleanup any partially uploaded assets
-function HandleErrorsAndCleanup {
-    param (
-        [int]$ExitCode
-    )
-    if ($ExitCode -eq 0) {
-        exit 0
-    }
-    if ($global:AssetIdsUploaded.Count -ne 0) {
-        Write-Host "`nCleaning up assets uploaded in the current execution of the script"
-        foreach ($assetId in $global:AssetIdsUploaded) {
-            Write-Host "Deleting asset $assetId"
-            Invoke-RestMethod -Method Delete -Uri "https://api.github.com/repos/LikithaVemulapalli/aws-node-termination-handler/releases/assets/$assetId" -Headers @{Authorization = "token $env:GITHUB_TOKEN"}
-        }
-        exit $ExitCode
-    }
-}
+# # Function to handle errors and cleanup any partially uploaded assets
+# function HandleErrorsAndCleanup {
+#     param (
+#         [int]$ExitCode
+#     )
+#     if ($ExitCode -eq 0) {
+#         exit 0
+#     }
+#     if ($global:AssetIdsUploaded.Count -ne 0) {
+#         Write-Output "`nCleaning up assets uploaded in the current execution of the script"
+#         foreach ($assetId in $global:AssetIdsUploaded) {
+#             Write-Output "Deleting asset $assetId"
+#             Invoke-RestMethod -Method Delete -Uri "https://api.github.com/repos/LikithaVemulapalli/aws-node-termination-handler/releases/assets/$assetId" -Headers @{Authorization = "token $env:GITHUB_TOKEN"}
+#         }
+#         exit $ExitCode
+#     }
+# }
 
 # Function to upload an asset to GitHub
 function UploadAsset {
@@ -39,31 +39,31 @@ function UploadAsset {
     
     try {
         $Response = Invoke-RestMethod -Method Post -Uri $Uri -Headers $Headers -InFile $AssetPath -ErrorAction Stop
-        if ($Response.id -ne $null) {
-            $global:AssetIdsUploaded += $Response.id
-            Write-Host "Created asset ID $($Response.id) successfully"
+        if ($Response -and $Response.id) {
+            #$global:AssetIdsUploaded += $Response.id
+            Write-Output "Created asset ID $($Response.id) successfully"
         } else {
-            Write-Host "❌ Upload failed with response message: $($Response | ConvertTo-Json) ❌"
+            Write-Output "❌ Upload failed with response message: $($Response | ConvertTo-Json) ❌"
             exit 1
         }
     } catch {
-        Write-Host "❌ Upload failed: $_"
+        Write-Output "❌ Upload failed: $_"
         exit 1
     }
 }
 
 # Initialize global variables
-$global:AssetIdsUploaded = @()
-trap { HandleErrorsAndCleanup -ExitCode $global:LASTEXITCODE }
+# $global:AssetIdsUploaded = @()
+# trap { HandleErrorsAndCleanup -ExitCode $global:LASTEXITCODE }
 
 $ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-$Version = & "$ScriptPath/../Makefile" version -s
+$Version = (& "$ScriptPath/../Makefile" version -s)
 $BuildDir = "$ScriptPath/../build/k8s-resources/$Version"
 $BinaryDir = "$ScriptPath/../build/bin"
 $ReleaseId = (Invoke-RestMethod -Uri "https://api.github.com/repos/LikithaVemulapalli/aws-node-termination-handler/releases" -Headers @{Authorization = "token $env:GITHUB_TOKEN"} | ConvertFrom-Json | Where-Object { $_.tag_name -eq $Version }).id
 
 if (-not $ReleaseId) {
-    Write-Host "❌ Failed to find release ID for version $Version ❌"
+    Write-Output "❌ Failed to find release ID for version $Version ❌"
     exit 1
 }
 
@@ -75,14 +75,14 @@ if (-not $BinariesOnly) {
 $Assets += Get-ChildItem -Path $BinaryDir | ForEach-Object { $_.FullName }
 
 # Log gathered assets
-Write-Host "Assets to upload:"
+Write-Output "Assets to upload:"
 foreach ($Asset in $Assets) {
-    Write-Host $Asset
+    Write-Output $Asset
 }
 
 # Upload each asset
 Write-Host "`nUploading release assets for release id '$ReleaseId' to Github"
 foreach ($Asset in $Assets) {
-    Write-Host "`n  Uploading $($Asset | Split-Path -Leaf)"
+    Write-Output "`n  Uploading $($Asset | Split-Path -Leaf)"
     UploadAsset -AssetPath $Asset
 }
